80a7be34310dd707815de2fdb0fbcd1e5dfa932a
kimyou1102 <yoosk5485@naver.com>
2026-06-26 05:34:39 +0900

    chore: Emotion 테마 환경 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
+import { ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { THEME } from './common/styles/theme';
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <App />
-    </QueryClientProvider>
+    <ThemeProvider theme={THEME}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
