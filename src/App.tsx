d050d15cc33124f0cff68ab67047be00811e598f
kimyou1102 <yoosk5485@naver.com>
2026-06-26 05:31:40 +0900

    chore: React Router 기본 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import { HomePage } from './pages/home/HomePage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+]);
+
 function App() {
-  return <div>igmo</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
