52ccb7fcf0dc4bcd9500433e6f7470612c80f1e9
kimyou1102 <yoosk5485@naver.com>
2026-06-26 06:00:29 +0900

    chore: ESLint와 Prettier 설정 추가

---
+++ eslint.config.js
@@ -0,0 +1,120 @@
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+import { fixupConfigRules } from '@eslint/compat';
+import { FlatCompat } from '@eslint/eslintrc';
+import js from '@eslint/js';
+import pluginQuery from '@tanstack/eslint-plugin-query';
+import tsParser from '@typescript-eslint/parser';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import importPlugin from 'eslint-plugin-import';
+import reactPlugin from 'eslint-plugin-react';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import unusedImportPlugin from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default defineConfig([
+  globalIgnores(['**/dist']),
+  ...pluginQuery.configs['flat/recommended'],
+  {
+    extends: fixupConfigRules(
+      compat.extends(
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react-hooks/recommended',
+      ),
+      prettier,
+    ),
+
+    plugins: {
+      'react-refresh': reactRefresh,
+      import: importPlugin,
+      'unused-imports': unusedImportPlugin,
+      react: reactPlugin,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+      },
+
+      parser: tsParser,
+    },
+
+    rules: {
+      'react-refresh/only-export-components': [
+        'warn',
+        {
+          allowConstantExport: true,
+        },
+      ],
+      'no-nested-ternary': 'error',
+      'no-console': ['warn', { allow: ['warn', 'error'] }],
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'react/self-closing-comp': 'error',
+
+      eqeqeq: ['error', 'always'],
+      'prefer-arrow-callback': 'warn',
+      '@typescript-eslint/consistent-type-imports': 'error',
+      'import/order': [
+        'error',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            'parent',
+            'sibling',
+            'index',
+            'object',
+            'type',
+          ],
+          pathGroups: [
+            {
+              pattern: 'react',
+              group: 'external',
+              position: 'before',
+            },
+            {
+              pattern: '**/pages/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '**/components/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '**/hooks/**',
+              group: 'internal',
+              position: 'after',
+            },
+            {
+              pattern: '**/utils/**',
+              group: 'internal',
+              position: 'after',
+            },
+          ],
+          pathGroupsExcludedImportTypes: ['react'],
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+          'newlines-between': 'always',
+        },
+      ],
+      'unused-imports/no-unused-imports': 'error',
+    },
+  },
+]);
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
+import { StrictMode } from 'react';
+
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
