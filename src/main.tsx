af244a7e478417627d21b84aede016d5444fd1b6
kimyou1102 <yoosk5485@naver.com>
2026-06-26 05:34:59 +0900

    style: Emotion reset 스타일 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
-import { ThemeProvider } from '@emotion/react';
+import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
+import { resetCss } from './common/styles/reset';
 import { THEME } from './common/styles/theme';
 
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <ThemeProvider theme={THEME}>
       <QueryClientProvider client={queryClient}>
+        <Global styles={[resetCss]} />
         <App />
       </QueryClientProvider>
     </ThemeProvider>
