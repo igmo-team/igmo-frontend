258da0cc07593891db69208b17c2e0ad95917eb7
kimyou1102 <yoosk5485@naver.com>
2026-07-02 02:24:48 +0900

    remove: 테스트 완료한 불필요한 코드 제거

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,11 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { HomePage } from './pages/home/HomePage';
-import { TestPage } from './pages/test/TestPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
-  },
-  {
-    path: '/test',
-    element: <TestPage />,
   },
 ]);
 
