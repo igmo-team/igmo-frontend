9ba8d2cf9a5b50c0686463f9371159a6734076eb
kimyou1102 <yoosk5485@naver.com>
2026-07-02 02:10:16 +0900

    feat: ai지침 테스트용 코드 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { HomePage } from './pages/home/HomePage';
+import { TestPage } from './pages/test/TestPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/test',
+    element: <TestPage />,
   },
 ]);
 
