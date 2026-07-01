380e49ba826594f001f5acc9aa7fcd475e329754
kimyou1102 <yoosk5485@naver.com>
2026-07-02 06:14:24 +0900

    feat: 로비 페이지 라우팅 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import { HomePage } from './pages/home/HomePage';
+import { LobbyPage } from './pages/lobby/LobbyPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <HomePage />,
+  },
+  {
+    path: '/lobby/:roomCode',
+    element: <LobbyPage />,
   },
 ]);
 
