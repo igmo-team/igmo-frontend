3c2b8d269389b6251c58468b5137c0fa8368f56d
kimyou1102 <yoosk5485@naver.com>
2026-07-13 05:10:25 +0900

    feat: 이미시 생성중 화면 임시디자인에서 스피너 추가한 디자인으로 변경

--- src/pages/room/components/RoomGeneratingView.tsx
+++ src/pages/room/components/RoomGeneratingView.tsx
@@ -1,22 +1,34 @@
 import styled from '@emotion/styled';
 
-// TODO: 실제 생성 진행 UI로 교체 (현재 임시 placeholder)
+import LoadingSpinner from '../../../common/components/LoadingSpinner';
+
 export function RoomGeneratingView() {
   return (
     <S_GeneratingSection>
       <S_Title>그림을 그리는 중이에요</S_Title>
-      <S_Guide>AI가 프롬프트를 열심히 그리고 있어요. 잠시만 기다려주세요.</S_Guide>
+      <S_Guide>
+        AI가 프롬프트를 열심히 그리고 있어요. 잠시만 기다려주세요.
+      </S_Guide>
+      <S_SpinnerBox>
+        <LoadingSpinner size="lg" />
+      </S_SpinnerBox>
     </S_GeneratingSection>
   );
 }
 
 const S_GeneratingSection = styled.section`
   display: flex;
+  min-height: calc(100dvh - 22rem);
   width: 100%;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 1.2rem;
   text-align: center;
+`;
+
+const S_SpinnerBox = styled.div`
+  margin-top: 2.8rem;
 `;
 
 const S_Title = styled.h2`
