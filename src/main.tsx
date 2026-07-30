11231a97c310adf4973d7bee739ca878cfc0cac4
kimyou1102 <yoosk5485@naver.com>
2026-07-30 18:00:15 +0900

    feat: 전역 토스트 렌더러 추가

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { StrictMode } from 'react';
 import { Global, ThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client';
+import { Toaster } from 'sonner';
 
 import App from './App';
 import { resetCss } from './common/styles/reset';
@@ -16,6 +17,11 @@ createRoot(document.getElementById('root')!).render(
       <QueryClientProvider client={queryClient}>
         <Global styles={[resetCss]} />
         <App />
+        <Toaster
+          duration={2400}
+          offset={{ bottom: 'var(--toast-bottom-offset, 9.6rem)' }}
+          position="bottom-center"
+        />
       </QueryClientProvider>
     </ThemeProvider>
   </StrictMode>,
