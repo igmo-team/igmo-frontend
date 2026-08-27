db62ff9b79af295593756e6515de60f02e315076
kimyou1102 <yoosk5485@naver.com>
2026-08-27 17:36:54 +0900

    chore: 배포 환경변수 누락 알림 검증 제거

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,7 +5,6 @@ interface ImportMetaEnv {
   readonly VITE_MIN_PLAYERS_TO_START: string;
   readonly VITE_POSTHOG_PROJECT_TOKEN: string;
   readonly VITE_POSTHOG_HOST: string;
-  readonly VITE_DEPLOY_NOTIFICATION_TEST: string;
 }
 
 interface ImportMeta {
